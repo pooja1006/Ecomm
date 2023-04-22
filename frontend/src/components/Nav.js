64c44b13a8b6712a6a645ae16a46f0da9e26a70a
Pooja Soni <poojasoni@Poojas-MacBook-Air.local>
2023-04-22 14:21:46 +0530

    backend commit

---
+++ frontend/src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Nav = () =>{
+    return(
+        <div>
+            <ul>
+                <li>Home page</li>
+            </ul>
+        </div>
+    )
+}
+export default Nav;
